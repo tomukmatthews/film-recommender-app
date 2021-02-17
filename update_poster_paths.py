4fbfdffe35409059707aa9e2c1034199a54a2b6b
Matthews <tom.matthews@touchsurgery.com>
2021-02-17 23:51:51 +0000

    Add main python files

---
+++ update_poster_paths.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from utils import update_poster_paths
+
+try:
+    dataframe = pd.read_csv("data/updated_poster_paths.csv")
+except:
+    dataframe = pd.read_csv("data/movies_metadata.csv")
+    # Remove rows with bad IDs.
+    dataframe = dataframe.drop([19730, 29503, 35587])
+    dataframe["id"] = dataframe["id"].astype("int")
+    dataframe = dataframe[["id", "poster_path"]]
+    dataframe["poster_path_updated"] = False
+
+updated_poster_paths = update_poster_paths(dataframe=dataframe, runtime_seconds=500)
+updated_poster_paths.to_csv("data/updated_poster_paths.csv", index=False)
